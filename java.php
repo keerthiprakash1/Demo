dbe845c9b74081fc64d8f0fd01783fc1ff3d0c66
Keerthi Prakash <Keerthip015@gmail.com>
2025-08-11 13:00:09 +0530

    files

---
+++ java.php
@@ -0,0 +1,28 @@
+package Package01;
+
+public class primitive_datatype {
+
+	public static void main(String[] args) {
+		//Declare variables of different data types
+		int number = 100;
+		float pi = 3.14f;
+		double bigDecimal = 12345.6789;
+		char letter = 'K';
+		boolean isJavaFun = true;
+		byte smallNumber = 10;
+		short shortNumber = 3000;
+		long bigNumber = 1000000L;
+
+		//Print the values
+		System.out.println("Integer Value: " + number);
+		System.out.println("Float Value: " + pi);
+		System.out.println("Double Value: " + bigDecimal);
+		System.out.println("Character Value: " + letter);
+		System.out.println("Boolean Value: " + isJavaFun);
+		System.out.println("Byte Value: " + smallNumber);
+		System.out.println("Short Value: " + shortNumber);
+		System.out.println("Long Value: " + bigNumber);
+
+	}
+
+}
